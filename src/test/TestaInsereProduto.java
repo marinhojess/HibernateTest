b4f98acec8aa1ac4a9bb33967341b59d7ed50181
marinhojess <jlmarinhocosta@gmail.com>
2017-07-31 10:57:28 -0300

    primeiro commit

---
+++ src/test/TestaInsereProduto.java
@@ -0,0 +1,23 @@
+package test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Produto;
+
+public class TestaInsereProduto {
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("lojavirtualDB");
+		EntityManager em = factory.createEntityManager();
+		
+		Produto p = new Produto();
+		p.setNome("Bola");
+		p.setPreco(40.00);
+		
+		em.getTransaction().begin();
+		em.persist(p);
+		em.getTransaction().commit();
+		
+	}
+}
